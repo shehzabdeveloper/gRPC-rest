42797e6223bafa4966ca27636ea9698b213cd980
Shehzab Ahammad <shehzabahammad@gmail.com>
2020-09-15 16:09:54 +0530

    proto updated

---
+++ client.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/shehzabdeveloper/gRPC-rest/echo"
+)
+
+func main() {
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dail(":9000", grpc.withInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+	defer conn.close()
+
+	c := echo.NewEchoServiceClient(conn)
+	response, err := c.Echo(context.Background(), &echo.EchoMessage{Value: "Check client"})
+	if err != nil {
+		log.Fatalf("Error when calling Echo:: %s", err)
+	}
+	log.Printf("Response from server: %s", response.Value)
+}
--- echo/echo.go
+++ echo/echo.go
@@ -10,6 +10,6 @@ type Server struct {
 }
 
 func (s *Server) Echo(ctx context.Context, in *EchoMessage) (*EchoMessage, error) {
-	log.Printf("Client message: %s", in.Value)
-	return &EchoMessage{Value: "Hello from Server"}, nil
+	log.Printf("Receive message body from client: %s", in.Value)
+	return &EchoMessage{Value: "Hello From the Server!"}, nil
 }
